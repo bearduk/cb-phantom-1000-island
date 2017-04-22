478c349a70ad481ff4d4d0b6461771d37e6ade26
Chris Beard <coebe500@aol.com>
2017-04-22 08:26:54 +0100

    :sunny: Initial commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+# [PhantomJS](http://phantomjs.org) - Scriptable Headless WebKit
+
+PhantomJS ([phantomjs.org](http://phantomjs.org)) is a headless WebKit scriptable with JavaScript.
+
+See screen.js under ./bin for the Island Scraper on github.
---
+++ bin/screen.js
@@ -0,0 +1,41 @@
+var page = require('webpage').create();
+
+var islandCount = 3;
+
+var sailing = function (num) {
+	console.log('Land ahoy!');// body...
+	page.open('https://pnewton84.github.io/island.html', function() {
+  		// page.render('island' + num + '.png');
+  		page.render('island-' + num + '.png');
+		// phantom.exit();
+	});
+}
+
+
+
+
+// for (var i = 0; i < islandCount; i++) {
+// 	console.log(islandCount[i]);
+// 	sailing(i);
+// };
+
+
+
+var started = Date.now();
+
+  // make it loop every 100 milliseconds
+  var interval = setInterval(function(){
+
+    // for x seconds
+    if (Date.now() - started > 3000000) {
+
+      // and then pause it
+      clearInterval(interval);
+
+    } else {
+
+      // the thing to do every 100ms
+	sailing(Date.now());
+
+    }
+  }, 500); // every x milliseconds
